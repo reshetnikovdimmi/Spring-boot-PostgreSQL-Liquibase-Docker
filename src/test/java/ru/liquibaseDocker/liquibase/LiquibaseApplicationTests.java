d27f4e78fc8977e07216346e5c4e0b6cd3813c04
DIMON <user357@rambler.ru>
2024-09-01 10:00:27 +0600

    the first fixation

---
+++ src/test/java/ru/liquibaseDocker/liquibase/LiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liquibaseDocker.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
